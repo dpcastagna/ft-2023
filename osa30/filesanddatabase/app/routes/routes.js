d2fe09696912626b5a06f2816f0ccdcf340802a5
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2023-05-29 18:30:32 +0300

    Delete osa30/filesanddatabase/app directory

--- osa30/filesanddatabase/app/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import * as formController from "./controllers/formController.js";
-
-const router = new Router();
-
-router.get("/", formController.viewForm);
-router.post("/", formController.sendFile);
-
-export { router };
